8f046a5b1a8e0c2ef3e00eca9e6d2368ed429777
30526 <taiburrahamanapu@gmail.com>
2025-10-15 19:08:28 +0600

    added search functionalities

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -3,14 +3,18 @@ import useProducts from '../../hooks/useProducts';
 import Product from '../../components/Product/Product';
 
 const Products = () => {
-    const [search, setSearch] = useState('')
-    const [products] = useProducts()
+
+    const [products] = useProducts();
+    const [search, setSearch] = useState('');
+    const term = search.trim().toLocaleLowerCase();
+    const searchedProducts = term ? products.filter(product => product.name.toLowerCase().includes(term)) : products;
+
     return (
         <div>
             <div className='flex justify-between items-center mb-10'>
                 <h2 className='text-3xl font-bold'>
                     See All Products
-                    <span className='ml-2 text-xs text-gray-500'>({products.length}) products found</span></h2>
+                    <span className='ml-2 text-xs text-gray-500'>({searchedProducts.length}) products found</span></h2>
                 <label className="input">
                     <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                         <g
@@ -18,8 +22,7 @@ const Products = () => {
                             strokeLinecap="round"
                             strokeWidth="2.5"
                             fill="none"
-                            stroke="currentColor"
-                        >
+                            stroke="currentColor">
                             <circle cx="11" cy="11" r="8"></circle>
                             <path d="m21 21-4.3-4.3"></path>
                         </g>
@@ -33,7 +36,7 @@ const Products = () => {
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
                 {
-                    products.map(product =>
+                    searchedProducts.map(product =>
                         <Product
                             key={product.id}
                             product={product}></Product>)
