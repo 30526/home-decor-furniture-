63831062c520080412ac1f302f7b482fb88dc926
30526 <taiburrahamanapu@gmail.com>
2025-10-15 22:36:58 +0600

    added sort functionalities to wishlist section

--- src/pages/WishList/WishList.jsx
+++ src/pages/WishList/WishList.jsx
@@ -2,6 +2,19 @@ import React, { useEffect, useState } from 'react';
 
 const WishList = () => {
     const [getItem, setGetItem] = useState([]);
+    const [sort, setSort] = useState('none');
+
+    const sortedItem = () => {
+        if (sort === 'asc') {
+            return [...getItem].sort((a, b) => a.price - b.price);
+        }
+        else if (sort === 'dsc') {
+            return [...getItem].sort((a, b) => b.price - a.price);
+        }
+        else {
+            return getItem;
+        }
+    }
 
     useEffect(() => {
         const getStorageItem = JSON.parse(localStorage.getItem('wishList'));
@@ -14,18 +27,19 @@ const WishList = () => {
                     Your Wish List
                     <span className='ml-2 text-xs text-gray-500'>({getItem.length}) products added</span></h2>
                 <div>
-                    <details className="dropdown">
-                        <summary className="btn m-1">Sort</summary>
-                        <ul className="menu dropdown-content bg-base-100 rounded-box z-1 p-2 shadow-sm">
-                            <li><a>Price</a></li>
-                            <li><a>Item</a></li>
-                        </ul>
-                    </details>
+                    <label className='form-control max-w-xs'>
+                        <select
+                            className='select' value={sort} onChange={(e) => setSort(e.target.value)}>
+                            <option value="none">Sort by Price</option>
+                            <option value="asc">Low to High</option>
+                            <option value="dsc">High to Low</option>
+                        </select>
+                    </label>
                 </div>
             </div>
-            <div className='mt-10 p-4 flex flex-col gap-4'>
+            <div className='mt-10 flex flex-col gap-4'>
                 {
-                    getItem.map(wish =>
+                    sortedItem().map(wish =>
                         <div key={wish.id} className='flex justify-between items-center p-4 shadow-2xs border-2 border-gray-100 rounded-2xl'>
                             <div className='flex gap-4'>
                                 <img className='w-fit h-35 rounded-2xl' src={wish.image} alt="" />
@@ -33,15 +47,14 @@ const WishList = () => {
                                     <h3 className='font-bold text-2xl'>{wish.name}</h3>
                                     <h3 className='mt-2 text-gray-500'>{wish.description}</h3>
                                     <h3 className='mt-2 text-gray-500'>{wish.category}</h3>
-                                    <p className='mt-2 text-gray-700 font-medium'>Price: ${wish.price}</p>
+                                    <p className='mt-2 text-gray-800 font-medium'>Price: ${wish.price}</p>
                                 </div>
                             </div>
                             <button className='btn btn-outline'>Remove</button>
-                        </div>)
-
-                }
+                        </div>
+                    )}
             </div>
-        </div>
+        </div >
     );
 };
 
