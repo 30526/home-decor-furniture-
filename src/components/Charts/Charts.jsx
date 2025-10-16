c2b42484bdec7416f082204e721e7db87ce34cd8
30526 <taiburrahamanapu@gmail.com>
2025-10-17 00:35:00 +0600

    added chart data validation

--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -2,21 +2,34 @@ import React from 'react';
 import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const Charts = ({ getItem }) => {
-    console.log(getItem)
+
+    const totalByCategory = {};
+
+    getItem.forEach(product => {
+        const category = product.category;
+        totalByCategory[category] =
+            (totalByCategory[category]) || 0 + product.price;
+    })
+
+    const chartData = Object.keys(totalByCategory).map(category => ({
+        category,
+        total: totalByCategory[category]
+    }))
+
     return (
-        <div>
+        <div className='mt-40'>
             <h4 className='my-10 text-center text-2xl font-bold'>WIsh List Summary</h4>
             <div className='bg-base-100 border border-xl p-4 h-80 rounded-2xl'>
                 <ResponsiveContainer width="100%" height="100%">
                     <BarChart
-                        data={getItem}
+                        data={chartData}
                     >
                         <CartesianGrid strokeDasharray="3 3" />
                         <XAxis dataKey="category" />
                         <YAxis />
                         <Tooltip />
                         <Legend />
-                        <Bar dataKey="price" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+                        <Bar dataKey="total" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
                     </BarChart>
                 </ResponsiveContainer>
             </div>
